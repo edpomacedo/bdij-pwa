d90b6c857ea9dff3faff9029a6e64c35065cb285
edpomacedo <edpomacedo@gmail.com>
2023-12-02 20:49:02 -0300

    Sidebar.jsx

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div>
       <Header />
+      <div class="container-fluid">
+        <div class="row mb-5">
+          <Sidebar />
+        </div>
+      </div>
     </div>
   );
 }

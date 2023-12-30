775f8faa654fbd87cdeaba0fe189da6f842831b7
edpomacedo <edpomacedo@gmail.com>
2023-12-29 22:11:48 -0300

    v0.1.4 (url)

--- src/layouts/Container/Container.jsx
+++ src/layouts/Container/Container.jsx
@@ -1,5 +1,5 @@
 // Container.jsx
-import React from "react";
+import React, { useEffect } from "react";
 import "./Container.css";
 import Hero from "../../components/Hero/Hero";
 import Services from "../../components/Services/Services"
@@ -7,6 +7,11 @@ import Stats from "../../components/Stats/Stats"
 import Providers from "../../components/Providers/Providers";
 
 function Container() {
+  useEffect(() => {
+    document.title = "Base de Dados de Institutos Jurídicos";
+    // Adicione mais lógica aqui, se necessário
+  }, []); // O array vazio assegura que o efeito só é executado uma vez durante a montagem do componente
+  
   return (
     <div className="fundo" data-testid="container">
       <div className="espacamento">
